27f4c6b49b892ac761d8415fe108dc391b596067
minleejae <mmj9808@naver.com>
2022-09-21 17:49:03 +0900

    Add .gitignore

---
+++ src/app.ts
@@ -0,0 +1,62 @@
+import express, { Request, Response, NextFunction } from "express";
+import myDataSource from "./app-data-source";
+import { User } from "./entity/user";
+
+// establish database connection
+myDataSource
+  .initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization:", err);
+  });
+
+const app = express();
+app.use(express.json());
+
+app.get("/welcome", (req: Request, res: Response, next: NextFunction) => {
+  res.send("welcome!!@");
+});
+
+// register routes
+app.get("/users", async function (req: Request, res: Response) {
+  const users = await myDataSource.getRepository(User).find();
+  res.json(users);
+});
+
+app.get("/users/:id", async function (req: Request, res: Response) {
+  const results = await myDataSource.getRepository(User).findOneBy({
+    id: Number(req.params.id),
+  });
+  return res.send(results);
+});
+
+app.post("/users", async function (req: Request, res: Response) {
+  const user = await myDataSource.getRepository(User).create(req.body);
+  const results = await myDataSource.getRepository(User).save(user);
+  return res.send(results);
+});
+
+app.put("/users/:id", async function (req: Request, res: Response) {
+  const user = await myDataSource.getRepository(User).findOneBy({
+    id: Number(req.params.id),
+  });
+  if (user === null) return;
+  myDataSource.getRepository(User).merge(user, req.body);
+  const results = await myDataSource.getRepository(User).save(user);
+  return res.send(results);
+});
+
+app.delete("/users/:id", async function (req: Request, res: Response) {
+  const results = await myDataSource.getRepository(User).delete(req.params.id);
+  return res.send(results);
+});
+
+app.listen("1234", () => {
+  console.log(`
+  ################################################
+  🛡️  Server listening on port: 1234🛡️
+  ################################################
+`);
+});
